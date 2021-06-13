9a76a1e226eea1b3c195229cb184ba28308ed4c8
Olivia Gao <oliviagao@olivias-mbp-3.lan>
2021-06-12 22:14:07 -0400

    stuff

--- script.js
+++ script.js
@@ -23,6 +23,8 @@ function timer(){
     } else if(h.value != 0 && m.value == 0){
         m.value = 60;
         h.value--;
+    } else if(h.value == 0 && m.value == 0 && s.value == 1) {
+        window.location.href = "https://google.com";
     }
     return;
 
