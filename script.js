d664e1f38d1965e3cb6f539a3a9a41a14a8837e6
nicolefeng2 <85264161+nicolefeng2@users.noreply.github.com>
2021-06-12 23:36:06 -0400

    timer redirect

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ function timer(){
         m.value = 60;
         h.value--;
     } else if(h.value == 0 && m.value == 0 && s.value == 1) {
-        window.location.href = "https://google.com";
+        <meta http-equiv="Refresh" content="0; url='https://www.w3docs.com'" />
     }
     return;
 }
